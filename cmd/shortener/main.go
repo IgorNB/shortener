dcb3af04079d24496f4b4883ca871c44c2982a3b
i.borisenko <i.borisenko@bronevik.com>
2026-04-19 01:50:36 +0300

    iter1: url shortener single node in memory naive implementation

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -8,12 +8,12 @@ import (
 	"github.com/IgorNB/shortener/internal/service"
 )
 
-const baseURL = "http://localhost:8080/"
+const BaseURL = "http://localhost:8080/"
 
 func main() {
 	repo := repository.New()
 	svc := service.New(repo)
-	h := handler.New(svc, baseURL)
+	h := handler.New(svc, BaseURL)
 
 	if err := http.ListenAndServe(":8080", h); err != nil {
 		panic(err)
