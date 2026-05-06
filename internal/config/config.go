57cd1a5070bb601710a64f14aa3875853e1f0672
i.borisenko <i.borisenko@bronevik.com>
2026-05-06 12:38:01 +0300

    iter5: env usage for conf added

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"flag"
+	"os"
 	"strings"
 )
 
@@ -14,6 +15,12 @@ func Parse() {
 	if !flag.Parsed() {
 		flag.StringVar(&ServerAddress, "a", "localhost:8080", "HTTP server address")
 		flag.StringVar(&BaseURL, "b", "http://localhost:8080/", "Base URL for short links")
+		if env, ok := os.LookupEnv("SERVER_ADDRESS"); ok {
+			ServerAddress = env
+		}
+		if env, ok := os.LookupEnv("BASE_URL"); ok {
+			BaseURL = env
+		}
 		flag.Parse()
 	}
 	if BaseURL != "" {
